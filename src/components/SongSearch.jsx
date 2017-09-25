1e727034d9947d8b1bcf8332691606565c9cab03
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-24 19:21:13 -0700

    add api call

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import styles from "./../styles/SongSearch.css";
+import { fetchSongId } from "./../actions";
+import { connect } from "react-redux";
 
-function SongSearch(){
-  let input;
+class SongSearch extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let { _title } = this.refs;
+    if (!_title.value.trim()) {
+      return;
+    }
+    this.props.dispatch(fetchSongId(_title.value.trim()));
+    _title.value = "";
+  }
+
+  render() {
   return (
     <div className = {styles.songSearch}>
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return;
-        }
-
-        // this is where custom logic goes
-        console.log("SEARCHED ARTIST:");
-        console.log(input.value.trim());
-        //
-
-        input.value = "";
-      }}>
-        <input placeholder="Song Title" ref={node => {
-          input = node;
-        }}></input>
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="Song Title" ref="_title"></input>
         <button>search</button>
       </form>
     </div>
   );
 }
+}
 
-export default SongSearch;
+export default connect()(SongSearch);
